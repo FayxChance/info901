0c45c0d49fbf7113fffd0324c3fe67ac00ea1ee8
Nicolas.C <comcomnico@gmail.com>
2021-09-16 16:34:05 +0200

    DiffusionCentralisee avec numéro de départ

--- DiffusionCentralisee.py
+++ DiffusionCentralisee.py
@@ -1,14 +1,31 @@
+# void BCast(from, msg) {
+#     if (myId == from) {
+#         for (i = 0; i < nbNodes-1; i++ ) {
+#             if (i != from)
+#                 send(i, msg)
+#         }
+#     } else {
+#         recieve(from, msg)
+#     }
+# }
+
 from mpi4py import MPI
+import sys
 
 comm = MPI.COMM_WORLD
 me = comm.Get_rank()
 size = comm.Get_size()
-print("Hi from <"+str(me)+">")
-if me == 0:
-    buf = ["coucou"]
-    print("I'm <"+str(me)+">: send " + buf[0])
-    for i in range(1, size):
-	    comm.send(buf, dest=i, tag=99)
-else:
-    buf = comm.recv(source=0, tag=99)
-    print("I'm <"+str(me)+">: receive " + buf[0])
+
+def broadcast(_from, msg):
+    if me == _from:
+        print("<"+str(me)+">: send " + msg)
+        for i in range(0, size):
+            if (i != _from):
+                comm.ssend(msg, dest=i, tag=99)
+    else:
+        buf = comm.recv(source=_from, tag=99)
+        print("<"+str(me)+">: receive " + buf[0])
+
+if __name__ == "__main__":
+    if(len(sys.argv) == 3):
+        broadcast(int(sys.argv[1]), sys.argv[2])
