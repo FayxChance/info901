3150994a0f3650d1e02a4c6b457d76b21d6c4716
FayxChance <fayxchancetom@outlook.com>
2021-09-20 11:10:44 +0200

    Update

--- TP2_Asynchrone_Bus/Process.py
+++ TP2_Asynchrone_Bus/Process.py
@@ -31,22 +31,66 @@ class Process(Thread):
         self.state = None
         self.start()
 
-    def get_Name(self):
+    def get_name(self):
         return int(self.getName())
 
-    @subscribe(threadMode=Mode.PARALLEL, onEvent=Message)
-    def onReceive(self, event):
-        if event.dest == self.get_Name():
-            self.compteur = self.compteur + 1 if self.compteur > event.cpt else event.cpt + 1
-            print(f"Worker {self.get_Name()} received message {event.msg}")
-
-
+    ############################   LAMPORT + BROADCAST   ############################
     @subscribe(threadMode=Mode.PARALLEL, onEvent=BroadcastMessage)
-    def onBroadcast(self, event):
-        if event.src != self.get_Name():
+    def on_broadcast(self, event):
+        if event.src != self.get_name():
             self.compteur = self.compteur + 1 if self.compteur > event.cpt else event.cpt + 1
-            print(f"Worker {self.get_Name()} received broadcasted message {event.msg}")
+            print(f"Worker {self.get_name()} received broadcasted message {event.msg}")
 
+    def broadcast(self, message):
+        self.compteur += 1
+        PyBus.Instance().post(BroadcastMessage(message.cpt + 1, message.msg, message.src))
+
+    ############################   LAMPORT + DEDICATED   ############################
+    @subscribe(threadMode=Mode.PARALLEL, onEvent=Message)
+    def on_receive(self, event):
+        if event.dest == self.get_name():
+            self.compteur = self.compteur + 1 if self.compteur > event.cpt else event.cpt + 1
+            print(f"Worker {self.get_name()} received message {event.msg}")
+
+    def send_to(self, obj):
+        self.compteur += 1
+        PyBus.Instance().post(obj)
+
+    ############################   TOKEN   ############################
+    @subscribe(threadMode=Mode.PARALLEL, onEvent=Token)
+    def on_token(self, event):
+        if self.get_name() == event.dest and self.alive:
+            sleep(1)
+            print(f"found token in {self.getName()}")
+            if self.state == "request":
+                print("i'm on request going on SC")
+                self.state = "SC"
+                while self.state != "release":
+                    sleep(1)
+            self.send_to(Token(event.id, (event.dest + 1) % 3))
+            print(f"token sent to next which is {(self.get_name() + 1) % 3}")
+            self.state = None
+
+    def request(self):
+        self.state = "request"
+        while self.state != "SC":
+            sleep(1)
+
+    def release(self):
+        self.state = "release"
+
+    ############################   SYNCHRONIZE   ############################
+    @subscribe(threadMode=Mode.PARALLEL, onEvent=Synchronisation)
+    def onSynchronize(self, event):
+        if event.src != self.get_name():
+            self.cptSynchronize -= 1
+
+    def synchronize(self):
+        PyBus.Instance().post(Synchronisation(self.get_name()))
+        while self.cptSynchronize > 0:
+            sleep(1)
+
+    ############################   RUN   ############################
     def run(self):
         loop = 0
         while self.alive:
@@ -75,9 +119,9 @@ class Process(Thread):
             #         print("releasing the beast !")
             #         self.release()
 
-            if self.get_Name() == 1 and loop == 0:
+            if self.get_name() == 1 and loop == 0:
                 sleep(5)
-            elif self.get_Name() == 2 and loop == 0:
+            elif self.get_name() == 2 and loop == 0:
                 sleep(10)
             if loop == 6:
                 self.synchronize()
@@ -88,49 +132,3 @@ class Process(Thread):
     def stop(self):
         self.alive = False
         self.join()
-
-    def sendTo(self, obj):
-        self.compteur += 1
-        PyBus.Instance().post(obj)
-
-    def sendTokenTo(self, token, to):
-        self.compteur += 1
-        PyBus.Instance().post(token)
-
-    def broadcast(self, message):
-        self.compteur += 1
-        PyBus.Instance().post(BroadcastMessage(message.cpt + 1, message.msg, message.src))
-
-    # TOKEN
-    def request(self):
-        self.state = "request"
-
-        while self.state != "SC":
-            sleep(1)
-
-    def release(self):
-        self.state = "release"
-
-    @subscribe(threadMode=Mode.PARALLEL, onEvent=Token)
-    def onToken(self, event):
-        if self.get_Name() == event.dest and self.alive:
-            sleep(1)
-            print(f"found token in {self.getName()}")
-            if self.state == "request":
-                print("i'm on request going on SC")
-                self.state = "SC"
-                while (self.state != "release"):
-                    sleep(1)
-            self.sendTo(Token(event.id, (event.dest + 1) % 3))
-            print(f"token sent to next which is {(self.get_Name() + 1) % 3}")
-            self.state = None
-
-    def synchronize(self):
-        PyBus.Instance().post(Synchronisation(self.get_Name()))
-        while self.cptSynchronize > 0:
-            sleep(1)
-
-    @subscribe(threadMode=Mode.PARALLEL, onEvent=Synchronisation)
-    def onSynchronize(self, event):
-        if event.src != self.get_Name():
-            self.cptSynchronize -= 1
